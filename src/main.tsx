bc68a134e0fa73015f96fa6f7c79ce402423e036
Noelia Donato <nsdonatovignais@teco.com.ar>
2022-09-21 18:29:12 -0300

    feat(app): remove dependencies and empty test

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import './index.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<App />}></Route>
-			</Routes>
-		</BrowserRouter>
+		<App />
 	</React.StrictMode>
 )
